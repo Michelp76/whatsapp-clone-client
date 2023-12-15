9761e74d76cb5b8e4d7cf8b17cbcaf17b5a4d100
michelp76 <michel.puissant@inextenso.fr>
2023-12-15 17:06:15 +0100

    Almost end of Part 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+  RouteComponentProps,
+} from 'react-router-dom';
+import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
 
 const App: React.FC = () => (
-  <div>
-    <ChatsListScreen />
-  </div>
+  <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/chats" component={ChatsListScreen} />
+        <Route
+          exact
+          path="/chats/:chatId"
+          component={({ match }: RouteComponentProps<{ chatId: string }>) => (
+            <ChatRoomScreen chatId={match.params.chatId} />
+          )}
+        />
+      </Switch>
+      <Route exact path="/" render={redirectToChats} />
+    </div>
+  </BrowserRouter>
 );
 
+const redirectToChats = () => <Redirect to="/chats" />;
 export default App;
