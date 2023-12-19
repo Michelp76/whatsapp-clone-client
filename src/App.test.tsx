4118c97aaf49b5a8ad273d936fa230fa6d4c2657
michelp76 <michel.puissant@inextenso.fr>
2023-12-19 13:55:36 +0100

    End of chapter 6

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,5 @@ import App from './App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   const root = createRoot(div!);
-  root.render(<App />);
+  root.render(<App />); 
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,32 @@ import {
   BrowserRouter,
   Route,
   Redirect,
-  Switch,
   RouteComponentProps,
 } from 'react-router-dom';
 import ChatRoomScreen from './components/ChatRoomScreen';
 import ChatsListScreen from './components/ChatsListScreen';
+import AnimatedSwitch from './components/AnimatedSwitch';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <div>
-      <Switch>
-        <Route exact path="/chats" component={ChatsListScreen} />
-        <Route
-          exact
-          path="/chats/:chatId"
-          component={({ match }: RouteComponentProps<{ chatId: string }>) => (
-            <ChatRoomScreen chatId={match.params.chatId} />
-          )}
-        />
-      </Switch>
-      <Route exact path="/" render={redirectToChats} />
-    </div>
+    <AnimatedSwitch>
+      <Route exact path="/chats" component={ChatsListScreen} />
+
+      <Route
+        exact
+        path="/chats/:chatId"
+        component={({ 
+          match,
+          history,
+        }: RouteComponentProps<{ chatId: string }>) => (
+          <ChatRoomScreen chatId={match.params.chatId} history={history} />
+        )}
+      />
+    </AnimatedSwitch>
+    <Route exact path="/" render={redirectToChats} />
   </BrowserRouter>
 );
 
 const redirectToChats = () => <Redirect to="/chats" />;
+
 export default App;
